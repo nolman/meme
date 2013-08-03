2fbbb1955aa576296d922a02279a4043606e6c0e
Nolan Evans <nolane@gmail.com>
2013-08-03 10:51:06 -0700

    improving specs

--- spec/controllers/memes_controller_spec.rb
+++ spec/controllers/memes_controller_spec.rb
@@ -3,34 +3,60 @@ require 'spec_helper'
 describe MemesController do
   render_views
 
-  let(:valid_attributes) { { name: "helloworld"} }
   let(:valid_session) {  }
-  let(:meme_image) { FactoryGirl.create(:meme_image) }
+  let!(:meme_image) { FactoryGirl.create(:meme_image) }
 
   describe "GET index" do
-    it "assigns all applications as @applications" do
+    it "responds successfully" do
       get :index, {}, valid_session
       response.should be_success
     end
   end
 
-  describe "GET show" do
-    it "assigns the requested application as @application" do
-      get :show, {:id => meme_image.to_param}, valid_session
-      response.should be_success
+  describe "POST create" do
+    it "creates a meme" do
+      expect {
+        post :create, {meme_image: {name: "hello world"}}, valid_session
+        response.should redirect_to(meme_path(MemeImage.last))
+      }.to change(MemeImage, :count).by(1)
     end
   end
 
-  describe "GET new" do
-    it "assigns the requested application as @application" do
-      get :new, {}, valid_session
+  describe "PUT update" do
+    it "updates a meme" do
+      expect {
+        put :update, {id: meme_image.id, meme_image: {name: "new name"}}, valid_session
+        response.should redirect_to(meme_path(meme_image))
+      }.to change {meme_image.reload.name}.from(meme_image.name).to("new name")
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes a meme image" do
+      expect {
+        delete :destroy, {id: meme_image.to_param}, valid_session
+        response.should redirect_to(memes_path)
+      }.to change(MemeImage, :count).by(-1)
+    end
+  end
+
+  describe "GET show" do
+    it "responds successfully" do
+      get :show, {id: meme_image.to_param}, valid_session
       response.should be_success
     end
   end
 
   describe "GET edit" do
-    it "assigns the requested application as @application" do
-      get :edit, {:id => meme_image.to_param}, valid_session
+    it "responds successfully" do
+      get :edit, {id: meme_image.to_param}, valid_session
+      response.should be_success
+    end
+  end
+
+  describe "GET new" do
+    it "responds successfully" do
+      get :new, {}, valid_session
       response.should be_success
     end
   end
